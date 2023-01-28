ae9aca72d7e581f5f98c805ec25ee21b13c431cb
Prince-1501 <princekumarrpcsvm10@gmail.com>
2023-01-28 13:05:23 +0530

    add files

--- generateQRCode/my-app/src/App.js
+++ generateQRCode/my-app/src/App.js
@@ -1,9 +1,44 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './styles/main.css'
 
 function App() {
   const [url, setUrl] = useState('');
   const [qrCode, setQrCode] = useState('');
+
+  const [image, setImage] = useState(null);
+  const [className, setClassName] = useState('');
+  const [probability, setProbability] = useState('');
+
+  const handleImageChange = (event) => {
+    setImage(event.target.files[0]);
+  }
+
+  const handleImageSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // Create a FormData object to send the image in the POST request
+      const formData = new FormData();
+      formData.append('image', image);
+
+      // Send the POST request
+      const response = await fetch('https://url-to-qr.onrender.com/recognize', {
+        method: 'POST',
+        body: formData
+      });
+
+      // Parse the JSON response
+      const jsonResponse = await response.json();
+
+      // Extract the first class name and probability from the response
+      const firstClass = jsonResponse[0];
+      setClassName(firstClass.className);
+      setProbability(firstClass.probability);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const handleChange = (event) => {
     setUrl(event.target.value);
@@ -32,7 +67,21 @@ function App() {
         {qrCode && <img src={qrCode} alt="QR Code" />}
       </div>
 
+    <h1 className="heading">Image Recognition</h1>
+      <form onSubmit={handleImageSubmit} className='form'>
+        <input type="file" onChange={handleImageChange} accept="image/*" className='input'/>
+        <button type="submit" className='button'>Recognize</button>
+      </form>
+
+      <div className="image-container">
+        {className && <p>Class Name: {className}</p>}
+        {probability && <p>Probability: {probability}</p>}
+        {image && <img src={URL.createObjectURL(image)} alt="Selected Image" />}
+      </div>
+
+
     </div>
+
   );
 }
 
